8a7300d9d3b099a6ca3f419ebcda122049a30492
Rodrigo Gomez <rodrigv31@gmail.com>
2017-09-03 00:53:30 -0300

    Agregados comentarios

--- Heladeria Tu Gusto/Heladeria Tu Gusto/RegistrarProducto.vb
+++ Heladeria Tu Gusto/Heladeria Tu Gusto/RegistrarProducto.vb
@@ -1,18 +1,20 @@
 ﻿Public Class RegistrarProducto
-
+    'Cadena de conexión generada por el visual studio
     Dim connection_string As String = "Provider=SQLNCLI11;Data Source=localhost\SQLExpress;Integrated Security=SSPI;Initial Catalog=HeladeriaTuGusto"
 
     Private Sub RegistrarProducto_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Al cargar el formulario, rellenar el comboBox de tipos de producto
         Me.cargarTiposProductos(cmb_tipo, leerTabla("TipoProducto"), "descripcion", "idTipo")
     End Sub
 
     Private Sub cmd_aceptar_Click(sender As Object, e As EventArgs) Handles cmd_aceptar.Click
+        'Llamar al procedimiento de inserción e informar que fue correcto.
         Me.insertar()
         MsgBox("Se ha guardado la información.", MsgBoxStyle.OkOnly, "¡Éxito!")
-
     End Sub
 
     Private Sub cargarTiposProductos(ByRef combo As ComboBox, ByRef tabla As DataTable, descripcion As String, pk As String)
+        'Sub rutina que rellena el comboBox con los elementos de una dataTable
         cmb_tipo.Items.Clear()
         cmb_tipo.DataSource = tabla
         cmb_tipo.DisplayMember = descripcion
@@ -20,6 +22,7 @@
     End Sub
 
     Private Function leerTabla(ByVal nombre_tabla As String) As DataTable
+        'Función que retorna una dataTable cargada con los resultados de la consulta SQL
         Dim conexion As New OleDb.OleDbConnection
         Dim comando As New OleDb.OleDbCommand
         Dim tabla As New Data.DataTable
@@ -36,6 +39,7 @@
     End Function
 
     Private Sub cmd_cancelar_Click(sender As Object, e As EventArgs) Handles cmd_cancelar.Click
+        'Simple funcionalidad del botón cancelar, limpia los campos y vuelve a mostrar la ventana principal
         If MessageBox.Show("Perderá los datos ingresados", "¿Desea cancelar el registro?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = Windows.Forms.DialogResult.Yes Then
             txt_descripcion.Clear()
             txt_nombre.Clear()
@@ -46,6 +50,10 @@
     End Sub
 
     Private Sub insertar()
+        'Procedimiento para la inserción de los datos en la BD:
+        'En el momento de declarar la sentencia SQL, se le van concatenando los valores
+        'tomados desde el formulario
+        'Sentencia = insert into Producto values (idProducto, 'nombre', idTipo, precio, stock, 'descripción')
         Dim conexion As New OleDb.OleDbConnection
         Dim comando As New OleDb.OleDbCommand
         Dim sql As String = ""
@@ -55,7 +63,7 @@
         comando.Connection = conexion
 
         sql = "insert into Producto values"
-        sql &= "(" & generarID()                        'idProducto
+        sql &= "(" & generarID()                        'idProducto, es generado automáticamente y no se repite
         sql &= ", '" & txt_nombre.Text.Trim & "'"       'nombre
         sql &= ", " & cmb_tipo.SelectedValue            'idTipo
         sql &= ", " & Integer.Parse(txt_precio.Text.Trim) 'precio
@@ -70,7 +78,7 @@
     End Sub
 
     Private Function generarID() As Integer
-        'Función que genera un número aleatorio entre 1 y 1000
+        'Función que genera un número aleatorio entre 100 y 1000
         Dim id As Integer
         Dim r As New Random
         id = r.Next(100, 1001)
@@ -84,7 +92,7 @@
     End Function
 
     Private Function idEstaDisponible(id As Integer) As Boolean
-
+        'Función auxiliar que verifica si el número generado en generarID() ya existe en la BD como una PK
         Dim conexion As New OleDb.OleDbConnection
         Dim cmd As New OleDb.OleDbCommand
         Dim tabla As New Data.DataTable
