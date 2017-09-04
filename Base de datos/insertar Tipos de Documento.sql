e4b7c422804312eb18ce65aca067e13fceac9a7a
Rodrigo Gomez <rodrigv31@gmail.com>
2017-09-04 20:18:11 -0300

    Actualización general

--- Base de datos/insertar Tipos de Documento.sql
+++
@@ -1,2 +0,0 @@
-insert into TipoDoc
-values (1, 'DNI'), (2, 'LE'), (3, 'CI'), (4, 'LC')
--- Base de datos/insertar Tipos de Producto.sql
+++
@@ -1,2 +0,0 @@
-insert into TipoProducto
-values (1, 'Torta Helada'), (2, 'Postre'), (3, 'Copa'), (4, 'Cono')
