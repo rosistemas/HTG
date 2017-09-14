2cebc9e68ff28ee70203ead60366a96b6d9f5642
rosistemas <rodrigv31@gmail.com>
2017-09-14 17:17:25 -0300

    Cambio en nombre de variables
    Cambios en los nombres de las variables del menu strip

--- Heladeria Tu Gusto/Heladeria Tu Gusto/Principal.vb
+++ Heladeria Tu Gusto/Heladeria Tu Gusto/Principal.vb
@@ -1,14 +1,25 @@
 ﻿Public Class Principal
-    Private Sub EmpleadoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EmpleadoToolStripMenuItem.Click
+    Private Sub registrar_empleado_Click(sender As Object, e As EventArgs) Handles registrar_empleado.Click
         RegistrarEmpleado.Show()
     End Sub
 
-    Private Sub ProductoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProductoToolStripMenuItem.Click
+    Private Sub registrar_producto_Click(sender As Object, e As EventArgs) Handles registrar_producto.Click
         RegistrarProducto.Show()
     End Sub
 
-    Private Sub ProveedorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProveedorToolStripMenuItem.Click
+    Private Sub registrar_proveedor_Click(sender As Object, e As EventArgs) Handles registrar_proveedor.Click
         RegistrarProveedor.Show()
     End Sub
 
+    Private Sub modificar_empleado_Click(sender As Object, e As EventArgs) Handles modificar_empleado.Click
+        ModificarEmpleado.Show()
+    End Sub
+
+    Private Sub modificar_proveedor_Click(sender As Object, e As EventArgs) Handles modificar_proveedor.Click
+        ModificarProveedor.Show()
+    End Sub
+
+    Private Sub modificar_producto_Click(sender As Object, e As EventArgs) Handles modificar_producto.Click
+        ModificarProducto.Show()
+    End Sub
 End Class
